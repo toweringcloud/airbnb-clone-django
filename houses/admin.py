58cdc448a6b33c9291242e385b5569aefa48cd33
toweringcloud <toweringcloud@gmail.com>
2024-09-20 23:56:34 +0900

    [add] common + users + rooms model

--- houses/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import House
-
-# Register your models here.
-@admin.register(House)
-class HouseAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "address", "pets_allowed")
-    list_filter = ("price", "pets_allowed")
-    search_fields = ("address__startswith", "name")
---
+++ rooms/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Room, Amenity
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
