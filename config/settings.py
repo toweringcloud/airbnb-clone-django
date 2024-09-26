46fe583019ee77f2684298dfe7a624f367c52a47
toweringcloud <toweringcloud@gmail.com>
2024-09-26 15:06:59 +0900

    models + : bookings, categories, direct_messages, experiences, medias, reviews, wishlists

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,13 @@ CUSTOM_APPS = [
     "common.apps.CommonConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "experiences.apps.ExperiencesConfig",
+    "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
+    "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
 SYSTEM_APPS = [
