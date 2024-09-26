e5bcd25870e085fa511b67d3b5ad3f52f1017949
toweringcloud <toweringcloud@gmail.com>
2024-09-26 15:31:19 +0900

    admin + : search_fileds, admin action, custom filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,14 +2,24 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (reset_prices,)
 
     list_display = (
         "name",
         "price",
         "kind",
         "total_amenities",
+        "rating",
         "owner",
         "created_at",
     )
@@ -22,6 +32,11 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
     )
 
 
