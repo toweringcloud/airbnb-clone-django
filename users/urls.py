a6a51834237d944b851f4c9b853a84254421b9b8
toweringcloud <toweringcloud@gmail.com>
2024-10-02 23:12:52 +0900

    user sig-up, sign-in and sign-out

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.Users.as_view()),
+    path("login", views.LogIn.as_view()),
+    path("logout", views.LogOut.as_view()),
     path("me", views.Me.as_view()),
+    path("password", views.ChangePassword.as_view()),
+    path("@<str:username>", views.UserDetail.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import User
 from .serializers import PrivateUserSerializer
 
 
@@ -27,3 +30,80 @@ class Me(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class Users(APIView):
+    def get(self, request):
+        all_users = User.objects.all()
+        serializer = PrivateUserSerializer(
+            all_users,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class UserDetail(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise NotFound
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise ParseError
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+
+
+class LogOut(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye!"})
